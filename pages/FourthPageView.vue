ebf653dda39397e7da881e4a5ba9e73747eceada
Amitai Salmon <amitai1997@gmail.com>
2021-01-26 21:46:51 +0200

    last working version

--- pages/FourthPageView.vue
+++ pages/FourthPageView.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <blockquote class="blockquote">
-        &#8220;this is my fourth page.&#8221;
-        <v-alert border="top" color="red lighten-2" dark>
-          I'm an alert with a top border and red color
-        </v-alert>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">OVERLINE</div>
+        <v-list-item-title class="headline mb-1">
+          Headline 5
+        </v-list-item-title>
+        <v-list-item-subtitle
+          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+        >
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text> Button </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
